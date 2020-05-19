eaf2b3245a363ba10c55c936b947763c7f0ecda4
Sami Laine <sami.v.laine@gmail.com>
2020-05-19 21:32:34 +0300

    Update message

--- embed.py
+++ embed.py
@@ -18,32 +18,31 @@ servers = \
 {0}
 """.format(indent)
 
-important_links = """{0} - [Attendance, missions & statistics](https://docs.google.com/spreadsheets/d/1obK3_FU5yMzhBgFLmk3g0v5dn5wFqT30RQXHWCAccCQ/edit)
-{0} - [Documentation](https://docs.google.com/document/d/1NKLi_VazqMgpnZYHa6FDOQbZyfqLDPosi4xdOQPgHMU/edit?usp=sharing)
-{0} - [Rules](https://docs.google.com/document/d/1DWK53LF3AjRqxZkb4qQXeBBvtSIOtamnUXyk2b6aaAs/edit?usp=sharing)
-{0} - [Zeus Guide](https://docs.google.com/document/d/1PFK__UcgmAJ1P3xBnJxeW2ow7u8bgEfM8lkpHJrLYDU/edit?usp=sharing)
-{0} - [Mission Templates (GitHub)](https://github.com/zeusops/mission-templates)
+important_links = """{0} - [Attendance (zeusops.com/attendance)](https://docs.google.com/spreadsheets/d/1obK3_FU5yMzhBgFLmk3g0v5dn5wFqT30RQXHWCAccCQ/edit)
+{0} - [Documentation (zeusops.com/doc)](https://docs.google.com/document/d/1NKLi_VazqMgpnZYHa6FDOQbZyfqLDPosi4xdOQPgHMU/edit?usp=sharing)
+{0} - [Rules (zeusops.com/rules)](https://docs.google.com/document/d/1DWK53LF3AjRqxZkb4qQXeBBvtSIOtamnUXyk2b6aaAs/edit?usp=sharing)
+{0} - [Zeus Guide (zeusops.com/zeus)](https://docs.google.com/document/d/1PFK__UcgmAJ1P3xBnJxeW2ow7u8bgEfM8lkpHJrLYDU/edit?usp=sharing)
+{0} - [Mission Templates (zeusops.com/template)](https://github.com/zeusops/mission-templates)
 {0}
 """.format(indent)
 
 mods = \
-"""{0} - [Mods Default](http://steamcommunity.com/sharedfiles/filedetails/?id=754233724)
-{0} - [Mods Lite](https://steamcommunity.com/sharedfiles/filedetails/?id=1123880053)
-{0} - [Mods Optional](https://steamcommunity.com/sharedfiles/filedetails/?id=1115077330)
-{0} - [Extra Terrain Pool](https://steamcommunity.com/sharedfiles/filedetails/?id=1424089214)
+"""{0} - [Mods Default (zeusops.com/mods)](http://steamcommunity.com/sharedfiles/filedetails/?id=754233724)
+{0} - [Mods Optional (zeusops.com/optional)](https://steamcommunity.com/sharedfiles/filedetails/?id=1115077330)
+{0} - [Extra Terrain Pool (zeusops.com/terrains)](https://steamcommunity.com/sharedfiles/filedetails/?id=1424089214)
 {0}
 """.format(indent)
 
 other_links = \
 """{0} - [Website](https://www.zeusops.com/)
-{0} - [Discord](https://www.zeusops.com/discord)
-{0} - [Steam](https://steamcommunity.com/groups/zeusops)
-{0} - [A3Units](https://units.arma3.com/unit/zeusops)
+{0} - [Discord (zeusops.com/discord)](https://www.zeusops.com/discord)
+{0} - [A3Units (zeusops.com/units)](https://units.arma3.com/unit/zeusops)
 {0} - [Twitter](https://twitter.com/zeusops)
-{0} - [Facebook](https://www.facebook.com/zeusoperations/)
 {0} - [Twitch](https://www.twitch.tv/communities/zeusoperations)
 {0} - [Patreon](https://www.patreon.com/zeusoperations)
 {0} - [PayPal](https://www.paypal.me/ZeusOperations)
+{0} - [Facebook](https://www.facebook.com/zeusoperations/)
+{0} - [Steam](https://steamcommunity.com/groups/zeusops)
 {0} - [Watch2Gether](https://www.watch2gether.com/rooms/zeusoperations-p7wbtfre05cwi65s)
 {0} - [TeamSpeak dark theme](https://www.myteamspeak.com/addons/686209af-0b66-4805-b2d7-0e990f7cb9e0)
 {0} - [TeamSpeak dark theme icons](https://drive.google.com/open?id=1b_zUlWf8tpUZijimgBK_olFxBUXNZ9V5)
